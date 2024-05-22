ad46896a0c72accfaa60e920fda8673c6a8a94e1
IoannisIn <intzes.ioannis@gmail.com>
2024-05-22 22:13:22 +0300

    Update awg.v

--- src/awg.v
+++ src/awg.v
@@ -1,10 +1,23 @@
-module awg (
-    input wire clk,
-    input wire rst,
-    input wire uart_rx,
-    output wire [9:0] waveform_data
-);
+module tt_um_awg (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+    );
 
+    assign clk = clk;
+    assign rst ~= rst_n;
+
+    assign uart_rx = ui_in[0];
+    assign uo_out = waveform_data[7:0];
+    assign uio_out[1:0] = waveform_data[9:8];
+    assign uio_oe  = 1;
+
+    
     wire [7:0] uart_data;
     wire data_valid;
     wire [1:0] waveform_type;
